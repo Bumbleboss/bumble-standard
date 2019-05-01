c1b730e26b55f6d497894f2cb5d80d8b40bcb385
Bumbleboss <bumbleboss46@gmail.com>
2019-05-01 04:23:00 +0300

    added a few more

--- bumble-standard.js
+++ bumble-standard.js
@@ -3,7 +3,14 @@ const math = require('./modules/math');
 let a = [2, 5]
 let b = [-3, 0]
 let c = [-4, 1]
+let v = [-2, 3]
 
 console.log(`component form --- ${math.findVectorComponentForm(a,b)}`);
 console.log(`vector length --- ${math.findVectorLength(a,b)}`)
-console.log(`vector addition --- ${math.addVector(a, b)}`)
+console.log(`vector length for one --- ${math.findVectorLength(v)}`)
+console.log(`vector addition --- ${math.addVector(c,a)}`)
+console.log(`vector subtraction --- ${math.subtractVector(b, a)}`)
+console.log(`vector multiplication --- ${math.multiplyVector(2, a)[0]}`)
+console.log(`unit vector --- ${math.findUnitVector(v)}`)
+
+
--- modules/math.js
+++ modules/math.js
@@ -1,25 +1,53 @@
 /*------------Unit 1: Vectors------------*/
 
 //finding the component form for a unit with a start and ending point in a set of array
-//usage: let a = [x1,y1], b = [x2,y2]
 exports.findVectorComponentForm = function(a, b) {
   return [(b[0]-a[0]), (b[1]-a[1])];
 }
 
-//finding the vector length using a starting and ending point in a set of array
+//finding the vector length using a starting and ending point in a set of array, also works unit vector
 //usage: let a = [x1,y1], b = [x2,y2]
-exports.findVectorLength = function(a, b) {
-  return Math.sqrt(Math.pow((b[0]-a[0]), 2) + Math.pow((b[1]-a[1]), 2))
+function findVectorLength() {
+  let arg = arguments;
+  if(arg.length == 1) {
+    return Math.sqrt(Math.pow(arg[0][0], 2) + Math.pow(arg[0][1], 2));
+  }else{
+    return Math.sqrt(Math.pow((arg[1][0]-arg[0][0]), 2) + Math.pow((arg[1][1]-arg[0][1]), 2));
+  }
 }
-
+exports.findVectorLength = findVectorLength;
 
 //addition in vectors, adding the vectors as an arguments in a set of array
 //usage: let a = [x1,y1], b = [x2,y2]
 exports.addVector = function() {
   let x = 0, y = 0, i = 0;
   for (;i < arguments.length; i++) {
-    x += arguments[i][0]
-    y += arguments[i][1]
+    x += arguments[i][0];
+    y += arguments[i][1];
   }
-  return [x,y]
+  return [x,y];
+}
+
+//subtraction in vectors, adding the vectors as an arguments in a set of array
+//usage: let a = [x1,y1], b = [x2,y2]
+exports.subtractVector = function() {
+  let i = 0, answer = [];
+  for (;i < arguments.length - 1; i++) {
+    answer.push([arguments[i][0] - arguments[i+1][0], arguments[i][1] - arguments[i+1][1]])
+  }
+  return answer[0];
+}
+
+//subtraction in vectors, adding the vectors as an arguments in a set of array
+//usage: let x = 2, b = [x1,y1]
+exports.multiplyVector = function() {
+  let i = 0, base = arguments[0], answer = [];
+  for (;i < arguments.length - 1; i++) {
+    answer.push([arguments[i+1][0]*base, arguments[i+1][1]*base]);
+  }
+  return answer;
+}
+
+exports.findUnitVector = function(v) {
+  return [v[0]/findVectorLength(v), v[1]/findVectorLength(v)]
 }
